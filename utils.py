68d43fa70295117a7a04dd174d5c4ece39967137
Josiel <josiel.borges@sc.senai.br>
2024-06-03 20:54:33 -0300

    Created an util class

---
+++ utils.py
@@ -0,0 +1,22 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+class LLMUtils() :
+
+    __temperature = 0
+    __model = ''
+    __api_key = os.getenv('OPENAI_API_KEY')
+
+    def __init__(self, temperature = 1, model = 'gpt-3.5-turbo'):
+        load_dotenv()
+        self.__temperature = temperature
+        self.__model = model
+        
+    def get_llm(self) -> ChatOpenAI:
+        llm = ChatOpenAI(
+            temperature = self.__temperature,
+            model = self.__model,
+            api_key = self.__api_key
+        )
+        return llm
