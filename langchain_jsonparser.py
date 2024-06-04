e0dd35d684af4375dbfab5d6387a6e705429dfa0
Josiel <josiel.borges@sc.senai.br>
2024-06-04 08:24:01 -0300

    Created an example of a json output parser

---
+++ langchain_jsonparser.py
@@ -0,0 +1,39 @@
+from langchain.prompts import PromptTemplate
+from langchain.chains import SimpleSequentialChain
+from langchain.chains import LLMChain
+from langchain_core.pydantic_v1 import Field, BaseModel
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.globals import set_debug # For debugging
+
+from utils import LLMUtils
+
+set_debug(True)
+
+class Destination(BaseModel):
+    city = Field("cidade a visitar")
+    reason = Field("motivo pelo qual é interessante visitar")
+
+llm = LLMUtils().get_openai_llm()
+
+output_parser = JsonOutputParser(pydantic_object=Destination)
+
+city_template = PromptTemplate(template='''
+                                        Sugira uma cidade dado meu interesse por {interest}.
+                                        {output_format}
+                                        ''',
+                                        input_variables=["interest"],
+                                        partial_variables={"output_format": output_parser.get_format_instructions()})
+
+# restaurant_template = ChatPromptTemplate.from_template("Sugira restaurantes populares entre locais em {city}")
+# cultural_template = ChatPromptTemplate.from_template("Sugira atividades e locais culturais em {city}")
+
+city_chain = LLMChain(prompt=city_template, llm=llm)
+# restaurant_chain = LLMChain(prompt=restaurant_template, llm=llm)
+# cultural_chain = LLMChain(prompt=cultural_template, llm=llm)
+
+chain = SimpleSequentialChain(chains=[city_chain
+                                    #   , restaurant_chain, cultural_chain
+                                      ], verbose = True)
+
+response = chain.invoke("praias")
+print(response)
