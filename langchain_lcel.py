d7696cb80d6d7f79667187abd0ec366477cbb931
Josiel <josiel.borges@sc.senai.br>
2024-06-04 11:22:26 -0300

    Improved code of langchain_lcel to include a final template e work better with returns and parallel chain

--- langchain_lcel.py
+++ langchain_lcel.py
@@ -2,6 +2,7 @@ from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.pydantic_v1 import Field, BaseModel
 from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
 from langchain.globals import set_debug # For debugging
+from operator import itemgetter
 
 from utils import LLMUtils
 
@@ -24,12 +25,27 @@ city_template = PromptTemplate(template='''
 
 restaurant_template = ChatPromptTemplate.from_template("Sugira restaurantes populares entre locais em {city}")
 cultural_template = ChatPromptTemplate.from_template("Sugira atividades e locais culturais em {city}")
+final_template = ChatPromptTemplate.from_messages(
+    [
+    ("ai", "Sugestão de viagem para a cidade: {city}"),
+    ("ai", "Restaurantes que você não pode perder: {restaurants}"),
+    ("ai", "Atividades e locais culturais recomendados: {cultural_locations}"),
+    ("system", "Combine as informações anteriores em 2 parágrafos coerentes")
+    ]
+)
 
 city_chain = city_template | llm | output_parser
 restaurant_chain = restaurant_template | llm | StrOutputParser()
 cultural_chain = cultural_template | llm | StrOutputParser()
+final_chain = final_template | llm | StrOutputParser()
 
-chain = city_chain | restaurant_chain | cultural_chain
+chain = (city_chain | 
+         {
+         "restaurants": restaurant_chain,
+         "cultural_locations": cultural_chain,
+         "city" : itemgetter("city")
+         } | 
+         final_chain)
 
 response = chain.invoke({"interest" : "praias"})
 print(response)
