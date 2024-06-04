cba575b231a051e4ad74c68aee2b381f98c03c92
Josiel <josiel.borges@sc.senai.br>
2024-06-04 16:53:18 -0300

    Included ollama at class that creates a llm object

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
 from dotenv import load_dotenv
 import os
 
@@ -20,3 +21,18 @@ class LLMUtils() :
             api_key = self.__api_key
         )
         return llm
+    
+    def get_ollama_llm(self) -> Ollama:
+        llm = Ollama(
+            temperature  = self.__temperature, 
+            model        = "phi3" 
+        )
+        return llm
+    
+    def get_llm(self, model):
+        if (model == 'openai'):
+            return self.get_openai_llm()
+        elif (model == 'ollama'):
+            return self.get_ollama_llm()
+        else:
+            raise Exception('Invalid model')
