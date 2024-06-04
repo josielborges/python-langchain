d283af64261b0b5a66683c09bb9eb2273ff5df79
Josiel <josiel.borges@sc.senai.br>
2024-06-04 16:08:49 -0300

    Created a example of a few_shot prompt

---
+++ longchain_fewshot.py
@@ -0,0 +1,35 @@
+from langchain.prompts import PromptTemplate
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.globals import set_debug # For debugging
+from utils import LLMUtils
+
+llm = LLMUtils().get_openai_llm()
+
+examples = [
+    {
+        "question": "Quem viveu mais, Muhammad Ali ou Alan Turing?",
+        "answer": """
+        São necessárias perguntas de acompanhamento: Sim.
+        Pergunta: Quantos anos Muhammad Ali tinha quando morreu?
+        Resposta intermediária: Muhammad Ali tinha 74 anos quando morreu.
+        Pergunta: Quantos anos Alan Turing tinha quando morreu?
+        Resposta intermediária: Alan Turing tinha 41 anos quando morreu.
+        Então a resposta final é: Muhammad Ali
+        """,
+    },
+]
+
+example_prompt = PromptTemplate(
+    input_variables=["question", "answer"], template="Pergunta: {question}\n{answer}"
+)
+
+prompt_template = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    suffix="Pergunta: {input}",
+    input_variables=["input"],
+)
+
+prompt = prompt_template.format(input="Quem foi o pai de Mary Ball Washington?")
+resposta = llm.invoke(prompt)
+print(resposta.content)
